f7e738e59a65d6561947ca1c1b080f875d3cc815
Ian DesJardins <ian.d.desjardins@gmail.com>
2020-04-07 22:40:48 -0400

    FIRST: component that just fetches with a button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FetchImageComponent } from './fetch-image/fetch-image.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'fetch',
+    component: FetchImageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/fetch-image/fetch-image.component.ts
+++ src/app/fetch-image/fetch-image.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fetch-image.component.sass']
 })
 export class FetchImageComponent implements OnInit {
+  imageUrl: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fetchImage() {
+    fetch('https://picsum.photos/1000')
+      .then(image => { this.imageUrl = image.url });
+  }
 }
